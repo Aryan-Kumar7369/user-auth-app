8d22b057b40542d55c3b2d6cbfe9e1c324e976fa
Aryan Kumar <aryankt2006@gmail.com>
2025-06-28 21:51:52 +0530

    Adding show password functionality.

---
+++ src/js/login.js
@@ -0,0 +1,13 @@
+
+function showPassFunction() {
+    var passType = document.getElementById("passField")
+    var showPassLogo = document.getElementsByClassName("showPass")[0]
+    
+    if (passType.type === "password") {
+        passType.type = "text"
+        showPassLogo.src = "images/eye-slash.png"
+    } else {
+        passType.type = "password"
+        showPassLogo.src = "images/eye-big.png"
+    }
+}
